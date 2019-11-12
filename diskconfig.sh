2f5e51de0c1351e956fe274c633bbff79c2c38ed
root user <root@domain.com>
2019-11-11 19:04:39 -0800

    Added RAID to diskconfig.sh* git log q

--- diskconfig.sh
+++ diskconfig.sh
@@ -11,3 +11,6 @@ echo -e "\n\n Partition Configuration: \n\n" >>$FILENAME.report
 fdisk -l | head -17 >>$FILENAME.report
 echo -e "\n\n Mounted Filesystem: \n\n" >>$FILENAME.report
 df -hT | grep -v tmp >>$FILENAME.report
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
