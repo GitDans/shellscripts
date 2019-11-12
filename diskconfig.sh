f4c43e65f516139b7de66d25602ef33a4b45afe7
root user <root@domain.com>
2019-11-11 19:12:55 -0800

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -14,3 +14,6 @@ df -hT | grep -v tmp >>$FILENAME.report
 
 echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
 mdadm --detail /dev/md0 >>$FILENAME.report
+
+echo -e "\n\n RAID configuration: \n\n" >> $FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
